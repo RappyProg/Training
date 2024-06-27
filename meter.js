e2dd5733a3f02c436238885ccba6589755c2273b
RappyProg <rappyprog@gmail.com>
2024-06-27 09:42:00 +0700

    build(meter) : practice converting centi to kilo

---
+++ meter.js
@@ -0,0 +1,8 @@
+// create a code to convert centimeter to kilometer
+//1km = 10000 cm
+
+let centi = 10000;
+let kilo = centi / (10 ** 4);
+
+console.log(`The distance in centimeter is ${centi}cm`);
+console.log(`The distance in kilometer is ${kilo}km`);
